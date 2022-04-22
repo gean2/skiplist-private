b8fe887d25e1bb8d9b78c1e9ada22eb81151a7c9
gean2 <gean@andrew.cmu.edu>
2022-04-22 14:48:49 -0400

    add test cases for test.cpp

--- test.cpp
+++ test.cpp
@@ -1,20 +1,21 @@
 #include "include/skiplist.h"
 //#include "include/synclist.h" // TODO: correct declaration generates linker errors -> why?
 #include "synclist.cpp" // TODO: WRONG RIGHT NOW
+#include "lockfree.cpp"
 #include <iostream>
-
 #include <assert.h>
 
 void add_test0(SkipList<int> *l) {
     int A[10] = {5,2,3,4,21,-2,33,12,6,7};
     int B[5] = {28, -4, 9, 10, 17};
     for(int i = 0; i < 10; i++) {
+        //std::cout << "adding " << A[i] << "\n";
         assert(l->update(A[i], &A[i]) == nullptr);
         assert(l->update(A[i], &A[i]) == &A[i]);
     }
     for(int i = 0; i < 5; i++) {
         assert(l->update(B[i], &B[i]) == nullptr);
-        assert(l->update(B[i], &B[i]) == &B[i]); // cannot add an element twice
+        assert(l->update(B[i], &B[i]) == &B[i]);
     }
     l->print();
     for(int i = 0; i < 10; i++) {
@@ -23,7 +24,9 @@ void add_test0(SkipList<int> *l) {
     for(int i = 0; i < 5; i++) {
         assert(l->lookup(B[i]) == &B[i]);
     }
+    //std::cout << "starting removal (oh no)\n";
     for(int i = 0; i < 5; i++) {
+        //std::cout << "removing " << B[i] << "\n";
         l->remove(B[i]);
     }
     for(int i = 0; i < 5; i++) {
@@ -39,5 +42,7 @@ void add_test0(SkipList<int> *l) {
 int main() {
     SyncList<int>l(4, 0.5);
     add_test0(&l);
+    LockFreeList<int> l2(4, 0.5);
+    add_test0(&l2);
     return 0;
 }
