7c58b9910fce6fd1c9975f87179fdd2e9da4608f
gean2 <gean@andrew.cmu.edu>
2022-05-03 12:57:02 -0400

    Add uniform keys for benchmarking

--- benchmark.cpp
+++ benchmark.cpp
@@ -46,17 +46,20 @@ int main(int argc, const char *argv[]) {
     double skip_prob = get_option_float("-p", 0.5f); // probability of increasing a level
     int max_height = get_option_int("-h", 20); // maximum height of skip list
 
-    bool no_sync = (bool) get_option_int("-ns", 0);
-    int num_trials = get_option_int("-r", 5);
-    int num_threads = get_option_int("-n", 8);
-    int array_length = get_option_int("-a", 10000000);
+    bool no_sync = (bool) get_option_int("-ns", 0); // do not run benchmark for synchronized skip list
+    int num_trials = get_option_int("-r", 5); // number of trials to average over
+    int num_threads = get_option_int("-n", 8); // number of threads to run
+    int array_length = get_option_int("-a", 10000000); // number of operations
+    int dist = get_option_int("-dist", 0); // distribution; 0 is uniform distribution; 1 is normal distribution
 
-    double update_prob = get_option_float("-i", 0.1f);
-    double removal_prob = get_option_float("-d", 0.1f);
-    int variance = get_option_float("-v", 100000);
+    double update_prob = get_option_float("-i", 0.1f); // probability of update operation
+    double removal_prob = get_option_float("-d", 0.1f); // probability of removal operation
+    int variance = get_option_int("-v", 100000); // parameter used for input distribution
 
     // compute inputs
-    std::vector<int> keys = generate_normal_keys(array_length, variance);
+    std::vector<int> keys;
+    if(dist) keys = generate_normal_keys(array_length, variance);
+    else keys = generate_uniform_keys(array_length, variance);
     std::vector<int> ops = generate_ops(array_length, update_prob, removal_prob);
 
     int max_deletions = std::count(ops.begin(), ops.end(), 2);
